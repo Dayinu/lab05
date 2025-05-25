908d26e62c02aad07dc21d8b2bcdf5898137869e
Kote <IvanovaVladaV@yandex.ru>
2025-05-25 10:05:46 -0400

    Update AccountTest.cpp

--- tests/AccountTest.cpp
+++ tests/AccountTest.cpp
@@ -1,21 +1,21 @@
+#include <Account.h>
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
-#include "../banking/Account.h"
 
-using ::testing::Return;
-using ::testing::_;
+TEST(Account, Banking){
+	Account test(0,0);
+	
+	ASSERT_EQ(test.GetBalance(), 0);
+	
+	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
+	
+	test.Lock();
+	
+	ASSERT_NO_THROW(test.ChangeBalance(100));
+	
+	ASSERT_EQ(test.GetBalance(), 100);
 
-class MockAccount : public Account {
-public:
-    MockAccount(int id, int balance) : Account(id, balance) {}
-    MOCK_METHOD(int, GetBalance, (), (const, override));
-    MOCK_METHOD(void, ChangeBalance, (int), (override));
-    MOCK_METHOD(void, Lock, (), (override));
-    MOCK_METHOD(void, Unlock, (), (override));
-};
+	ASSERT_THROW(test.Lock(), std::runtime_error);
 
-TEST(AccountTest, LockThrowsIfAlreadyLocked) {
-    MockAccount acc(1, 1000);
-    EXPECT_CALL(acc, Lock()).WillOnce(testing::Throw(std::runtime_error("already locked")));
-    ASSERT_THROW(acc.Lock(), std::runtime_error);
+	test.Unlock();
+	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
 }
