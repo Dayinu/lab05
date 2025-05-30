c9b0030a8a863ddefe830c30a88460998eac38e6
Kote <IvanovaVladaV@yandex.ru>
2025-05-30 18:37:55 +0300

    Update AccountTest.cpp

--- tests/AccountTest.cpp
+++ tests/AccountTest.cpp
@@ -1,6 +1,6 @@
-#include <Account.h>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include "banking/Account.h"
 
 class MockAccount : public Account {
 public:
@@ -12,38 +12,16 @@ public:
     MOCK_METHOD(void, Unlock, (), (override));
 };
 
-TEST(AccountTest, MockTest) {
+TEST(AccountTest, BalanceTest) {
     MockAccount acc(1, 1000);
     
     EXPECT_CALL(acc, GetBalance())
         .Times(1)
-        .WillOnce(::testing::Return(1000));
+        .WillOnce(testing::Return(1000));
     
     EXPECT_CALL(acc, Lock())
         .Times(1);
     
-    EXPECT_CALL(acc, ChangeBalance(500))
+    EXPECT_CALL(acc, ChangeBalance(1000))
         .Times(1);
-    
-    EXPECT_CALL(acc, Unlock())
-        .Times(1);
-    
-    ASSERT_EQ(acc.GetBalance(), 1000);
-    acc.Lock();
-    acc.ChangeBalance(500);
-    acc.Unlock();
-}
-
-TEST(AccountTest, Banking) {
-    Account test(0,0);
-    ASSERT_EQ(test.GetBalance(), 0);
-    ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
-    
-    test.Lock();
-    ASSERT_NO_THROW(test.ChangeBalance(100));
-    ASSERT_EQ(test.GetBalance(), 100);
-    ASSERT_THROW(test.Lock(), std::runtime_error);
-    
-    test.Unlock();
-    ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
 }
